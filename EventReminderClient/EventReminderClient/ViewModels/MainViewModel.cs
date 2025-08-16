726ade3e29804932c7406f29c4b7ef9783cd15a7
Volodymyr Bezshtanko <v.v.bezshtanko@gmail.com>
2025-08-16 20:22:24 +0300

    purge repo

--- EventReminderClient/EventReminderClient/ViewModels/MainViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace EventReminderClient.ViewModels;
-
-public partial class MainViewModel : ViewModelBase
-{
-    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
-}
