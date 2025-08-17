ac0bc1a0318281e9268aaa1d2e96973f164d36eb
Volodymyr Bezshtanko <v.v.bezshtanko@gmail.com>
2025-08-17 10:06:30 +0300

    finish profile page

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -69,6 +69,17 @@
         <option value="+01:00">UTC+01:00</option>
         <option value="+02:00">UTC+02:00</option>
         <option value="+03:00">UTC+03:00</option>
+        <option value="+04:00">UTC+04:00</option>
+        <option value="+05:00">UTC+05:00</option>
+        <option value="+06:00">UTC+06:00</option>
+        <option value="+07:00">UTC+07:00</option>
+        <option value="+08:00">UTC+08:00</option>
+        <option value="+09:00">UTC+09:00</option>
+        <option value="+10:00">UTC+10:00</option>
+        <option value="+11:00">UTC+11:00</option>
+        <option value="+12:00">UTC+12:00</option>
+        <option value="+13:00">UTC+13:00</option>
+        <option value="+14:00">UTC+14:00</option>
         <option value="-01:00">UTC-01:00</option>
         <option value="-02:00">UTC-02:00</option>
         <option value="-03:00">UTC-03:00</option>
@@ -81,17 +92,6 @@
         <option value="-10:00">UTC-10:00</option>
         <option value="-11:00">UTC-11:00</option>
         <option value="-12:00">UTC-12:00</option>
-        <option value="+04:00">UTC+04:00</option>
-        <option value="+05:00">UTC+05:00</option>
-        <option value="+06:00">UTC+06:00</option>
-        <option value="+07:00">UTC+07:00</option>
-        <option value="+08:00">UTC+08:00</option>
-        <option value="+09:00">UTC+09:00</option>
-        <option value="+10:00">UTC+10:00</option>
-        <option value="+11:00">UTC+11:00</option>
-        <option value="+12:00">UTC+12:00</option>
-        <option value="+13:00">UTC+13:00</option>
-        <option value="+14:00">UTC+14:00</option>
         <option value="-13:00">UTC-13:00</option>
         <option value="-14:00">UTC-14:00</option>
     </select>
