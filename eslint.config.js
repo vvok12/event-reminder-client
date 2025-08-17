3bb9b783f7d7b4b59cce8619ef9062ce48bf36b8
Volodymyr Bezshtanko <v.v.bezshtanko@gmail.com>
2025-08-17 09:06:15 +0300

    drop storybook for now

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from 'eslint-plugin-storybook';
-
 import prettier from 'eslint-config-prettier';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
@@ -39,6 +36,5 @@ export default ts.config(
 				svelteConfig
 			}
 		}
-	},
-	storybook.configs['flat/recommended']
+	}
 );
